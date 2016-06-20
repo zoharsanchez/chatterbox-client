6a3d295d817b569b4b1ec563f8965acdd4047a38
zoharsanchez <piccola.lupina@gmail.com>
2016-06-20 15:18:43 -0700

    Has methods init, send, fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,45 @@
 // YOUR CODE HERE:
+var app = {
+  message: {
+    username: 'Zohandrew',
+    text: 'Testing'
+    // roomname: '4chan'
+  },
+  server: 'https://api.parse.com/1/classes/messages'
+};
 
+app.init = function() {};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent', message);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  return $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received', data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
